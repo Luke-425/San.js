0385b6733304ee88b900185106a3a7c9739f7995
Luke_425 <garcia.luc66@gmail.com>
2022-05-01 15:24:21 +0200

    Fullscreen and resizing

--- src/script.js
+++ src/script.js
@@ -1,17 +1,6 @@
 import './style.css';
 import * as THREE from 'three';
-
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-
-// Cursor
-const cursor = {
-  x: 0,
-  y: 0,
-};
-window.addEventListener('mousemove', (event) => {
-  cursor.x = event.clientX / sizes.width - 0.5;
-  cursor.y = -(event.clientY / sizes.height - 0.5);
-});
 
 /**
  * Base
@@ -19,76 +8,85 @@ window.addEventListener('mousemove', (event) => {
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
 
-// Sizes
-const sizes = {
-  width: 800,
-  height: 600,
-};
-
 // Scene
 const scene = new THREE.Scene();
 
-// Object
-const mesh = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-  new THREE.MeshBasicMaterial({ color: 'blue' })
-);
+/**
+ * Object
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-// Camera
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+window.addEventListener('dblclick', () => {
+  // For SAFARI compatibility
+  const fullscreenElement =
+    document.fullscreenElement || document.webkitFullscreenElement;
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) canvas.requestFullscreen();
+    else if (canvas.webkitFullscreenElement) canvas.webkitFullscreenElement();
+  } else {
+    if (document.exitFullscreen) document.exitFullscreen();
+    else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+  }
+});
+
+/**
+ * Camera
+ */
+// Base camera
 const camera = new THREE.PerspectiveCamera(
   75,
   sizes.width / sizes.height,
   0.1,
   100
 );
-
-// const aspectRatio = sizes.width / sizes.height;
-// console.log(aspectRatio);
-// const camera = new THREE.OrthographicCamera(
-//   -1 * aspectRatio,
-//   1 * aspectRatio,
-//   1,
-//   -1,
-//   0.1,
-//   100
-// );
-
-// camera.position.x = 2
-// camera.position.y = 2
 camera.position.z = 3;
-// console.log(camera.position.length())
-camera.lookAt(mesh.position);
 scene.add(camera);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
+// controls.enabled = false;
 controls.enableDamping = true;
-// controls.target.y = 1;
-// controls.update();
 
-// Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
 
-// Animate
+/**
+ * Animate
+ */
 const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
-
-  // Update object
-  // mesh.rotation.y = elapsedTime;
-
-  // Update camera
-  // camera.rotation.x = cursor.y;
-  // camera.rotation.y = cursor.x;
-  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-  // camera.position.y = cursor.y * 5;
-  // camera.lookAt(mesh.position);
 
   // Update controls
   controls.update();
